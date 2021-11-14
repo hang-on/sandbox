ae8c106f543eca88d6de3c2365c2b27755ccca9f
hang-on <hang-on@users.noreply.github.com>
2021-11-14 12:17:58 +0100

    Make map lib

--- libraries/map_lib.asm
+++ libraries/map_lib.asm
@@ -1,1 +1,208 @@
 ; Library to interface with map (Tiled, converter etc.)
+; -----------------------------------------------------------------------------
+; Map
+; Note: This lib needs access to quite some tables. Best placed in bank 1.
+; -----------------------------------------------------------------------------
+.equ MAP_WIDTH 16         ; Width in metatiles.
+.equ MAP_HEIGHT 10        ; Height in metatiles.
+; -----------------------------------------------------------------------------
+.ramsection "Map Variables" slot 3
+; -----------------------------------------------------------------------------
+  tile_buffer dsb 20
+  metatile_buffer dsb 10
+  map_head dw
+  nametable_head db
+  metatile_halves db          ; Convert left or right half of the metatile to tiles.
+  load_nametable_column db    ; Flag.
+
+.ends
+
+
+
+; -----------------------------------------------------------------------------
+.section "Map" free
+; -----------------------------------------------------------------------------   
+
+  convert_next_half_of_metatile_column:
+    ld a,(metatile_halves)
+    cp 0
+    jp nz,+
+      call convert_left_half_of_metatile_column
+      jp ++
+    +:
+      call convert_right_half_of_metatile_column
+      ; Add here: Read next metatile column into the metatile buffer.
+    ++:
+    ld a,(metatile_halves)
+    cpl
+    ld (metatile_halves),a
+  ret
+
+
+  write_next_column:
+    ld a,(nametable_head)
+    ld h,0
+    ld l,a
+    add hl,hl
+    ld de,column_loader_table
+    add hl,de
+    ld a,(hl)
+    inc hl
+    ld h,(hl)
+    ld l,a
+    call function_at_hl
+    ; Forward the name table head:
+    ld a,(nametable_head)
+    cp 31
+    jp nz,+
+      xor a
+      jp ++
+    +:
+      inc a
+    ++:
+    ld (nametable_head),a
+    ld a,FALSE
+    ld (load_nametable_column),a
+  ret
+
+.ends
+
+.bank 1 slot 1
+ ; ----------------------------------------------------------------------------
+.section "Tables" free
+; -----------------------------------------------------------------------------
+  column_loader_table:
+    ; To be indexed by the variable name_table_head.
+    .dw load_column_0
+    .dw load_column_1
+    .dw load_column_2
+    .dw load_column_3
+    .dw load_column_4
+    .dw load_column_5
+    .dw load_column_6
+    .dw load_column_7
+    .dw load_column_8
+    .dw load_column_9
+    .dw load_column_10
+    .dw load_column_11
+    .dw load_column_12
+    .dw load_column_13
+    .dw load_column_14
+    .dw load_column_15
+    .dw load_column_16
+    .dw load_column_17
+    .dw load_column_18
+    .dw load_column_19
+    .dw load_column_20
+    .dw load_column_21
+    .dw load_column_22
+    .dw load_column_23
+    .dw load_column_24
+    .dw load_column_25
+    .dw load_column_26
+    .dw load_column_27
+    .dw load_column_28
+    .dw load_column_29
+    .dw load_column_30
+    .dw load_column_31
+
+  ; Convert left half of a column of metatiles to tiles in the buffer.
+  convert_left_half_of_metatile_column:
+    .rept 10 INDEX COUNT
+      ld a,(metatile_buffer+COUNT)
+      ld hl,top_left_corner ;
+      call lookup_byte      ; 
+      ld (tile_buffer+(COUNT*2)),a
+      ld a,(metatile_buffer+COUNT)
+      ld hl,bottom_left_corner
+      call lookup_byte      
+      ld (tile_buffer+(COUNT*2)+1),a
+    .endr
+  ret
+
+  ; Convert right half of a column of metatiles to tiles in the buffer.
+  convert_right_half_of_metatile_column:
+    .rept 10 INDEX COUNT
+      ld a,(metatile_buffer+COUNT)
+      ld hl,top_right_corner ;
+      call lookup_byte      ; 
+      ld (tile_buffer+(COUNT*2)),a
+      ld a,(metatile_buffer+COUNT)
+      ld hl,bottom_right_corner
+      call lookup_byte      
+      ld (tile_buffer+(COUNT*2)+1),a
+    .endr
+  ret
+  
+  top_left_corner:
+  ; ID 0 1 2 3 4 5  6  7  8  9  10 11 12 13 14 15
+   .db 0 2 4 6 8 10 12 14 16 18 20 22 24 26 28 30
+  ; ID 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 
+   .db 64 66 68 70 72 74 76 78 80 82 84 86 88 90 92 94
+  ; ID 32  33  34  35  36  37  38  39  40  41  42  43  44  45  46  47 
+   .db 128 130 132 134 136 138 140 142 144 146 148 150 152 154 156 158
+
+  bottom_left_corner:
+    .rept 16 INDEX COUNT
+      ; ID 0-15
+      .db 32+(COUNT*2)
+    .endr 
+    .rept 16 INDEX COUNT
+      ; ID 16-31
+      .db 96+(COUNT*2)
+    .endr 
+    .rept 16 INDEX COUNT
+      ; ID 32-47
+      .db 160+(COUNT*2)
+    .endr 
+
+  top_right_corner:
+    .rept 16 INDEX COUNT
+      ; ID 0-15
+      .db 1+(COUNT*2)
+    .endr 
+    .rept 16 INDEX COUNT
+      ; ID 16-31
+      .db 65+(COUNT*2)
+    .endr 
+    .rept 16 INDEX COUNT
+      ; ID 32-47
+      .db 129+(COUNT*2)
+    .endr 
+
+  bottom_right_corner:
+    .rept 16 INDEX COUNT
+      ; ID 0-15
+      .db 33+(COUNT*2)
+    .endr 
+    .rept 16 INDEX COUNT
+      ; ID 16-31
+      .db 97+(COUNT*2)
+    .endr 
+    .rept 16 INDEX COUNT
+      ; ID 32-47
+      .db 161+(COUNT*2)
+    .endr 
+
+  ; Unrolled loops to quickly load a name table column from the buffer.
+  .macro COLUMN_LOADER ARGS ADDRESS
+    load_column_\@:
+      .rept 20 INDEX COUNT
+        ld hl,ADDRESS+COUNT*64
+        ld a,l
+        out (CONTROL_PORT),a
+        ld a,h
+        or VRAM_WRITE_COMMAND
+        out (CONTROL_PORT),a
+        ld a,(tile_buffer+COUNT)
+        out (DATA_PORT),a   
+        ld a,%00000001
+        out (DATA_PORT),a
+      .endr
+    ret
+  .endm
+  .rept 32 INDEX COLUMN
+    COLUMN_LOADER $3880+(COLUMN*2)
+  .endr
+
+.ends
